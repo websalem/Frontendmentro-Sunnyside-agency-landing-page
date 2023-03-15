c7823713fd400e60b1d306ee36a581720dd18dbe
Ahmed Salem <ahmed.salem375@hotmail.com>
2023-03-15 13:29:30 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <>
       <Routes>
 
-        <Route exact path='/' element={<Home />} />
+        <Route exact path='/index.html' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/services' element={<Services />} />
         <Route path='/projects' element={<Projects />} />
