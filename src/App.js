ced30779654847419980ad037cc6b454afa27676
Ahmed Salem <ahmed.salem375@hotmail.com>
2023-03-15 14:25:03 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Routes>
 
           <Route path='/' element={<Home />} />
