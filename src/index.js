5e8a789f10fba5ee07b7cc53c77655807ddc3cae
Ahmed Salem <ahmed.salem375@hotmail.com>
2023-03-15 14:05:39 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-
-    <App />
-
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>
 );
 
